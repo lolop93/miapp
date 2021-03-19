9131765bbac2f5bd9e806aa6a7ea1bb63a8a58c3
lolop93 <lolop_1993@hotmail.com>
2021-03-19 02:26:32 +0100

    Popup casi finalizado

--- src/components/PopupLogin.vue
+++ src/components/PopupLogin.vue
@@ -1,22 +1,27 @@
 <template lang="html">
 
-  <button @click="estado = true" class="boton-login botlog">
-    Abrete puto
+  <div class="container containerpadre">
 
-  </button>
-  <div class="container-relative" style="position:relative">
-    <div v-if="estado" class="modal-container">
-      <div class="button-container">
-        <hr>
-        <a href="#">Iniciar Sesión</a>
-        <hr>
-        <a href="#">Registrarse</a>
-        <hr>
-        <a @click="estado = false">Cerrar</a>
-        <hr>
+      <button @click="estado = true" class="boton-login botlog">
+        Abrete puto
+
+      </button>
+      <div class="" style="position:relative">
+        <div v-if="estado" class="modal-container">
+          <div class="button-container">
+            <hr>
+            <a href="#">Iniciar Sesión</a>
+            <hr>
+            <a href="#">Registrarse</a>
+            <hr>
+            <a @click="estado = false">Cerrar</a>
+            <hr>
+          </div>
+        </div>
       </div>
-    </div>
+
   </div>
+
 
 
 
@@ -40,11 +45,13 @@ export default {
 
 <style lang="css" scoped>
 
+.containerpadre{
+  max-width: 200px;
+}
 .boton-login{
   background-color: orange; /* Green */
   border: none;
   color: white;
-  padding: 16px 32px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
@@ -56,7 +63,6 @@ export default {
 .botlog {
   background-color: white;
   color: black;
-  min-height: 50px;
   border: orange solid 1px;
   border-radius: 5px;
 }
@@ -73,20 +79,15 @@ export default {
 	background: orange;
 	border-radius: .4em;
   margin-top: 15px;
+  width: 100%;
 
-
-}
-
-.container-relative{
-    margin: auto 0;
 }
 
 .modal-container a{
-  margin: 20px;
+  margin: 0px;
   text-decoration: none;
   display: block;
   cursor: pointer;
-
 }
 
 </style>
